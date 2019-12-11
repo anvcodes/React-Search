ce92cd31d58844cab63c9810693d3bdb84b3f951
anvcodes <kpeace0720@gmail.com>
2019-12-10 19:09:16 -0500

    updates

---
+++ models/savedBooks.js
@@ -0,0 +1,17 @@
+const mongoose =require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let SavedBooksSchema = new Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    name: String,
+    author: String,
+    description:String,
+    image:String,
+    link:String,
+    bookId:String
+});
+
+let SavedBooks = mongoose.model("SavedBooks", SavedBooksSchema);
+
+module.exports = SavedBooks;
